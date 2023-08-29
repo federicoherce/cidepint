02cb942249ef1942920201e19e812f869cb48507
Federico Herce <federicoherce25@gmail.com>
2023-08-29 16:17:52 -0300

    adding multiplication

--- src/calculator.py
+++ src/calculator.py
@@ -7,4 +7,6 @@ def calculate():
 
     print("Suma: ", operations.add(num1,num2))
     print("Resta: ", operations.subtract(num1,num2))
+    print("Multiplicacion: ", operations.multiply(num1, num2))
     print("División: ", operations.divide(num1, num2))
+    
--- src/operations.py
+++ src/operations.py
@@ -6,8 +6,13 @@ def subtract(a,b):
     return a - b
 
 
+def multiply(a,b):
+    return a * b
+
+
 def divide(a, b):
     if b != 0:
         return a / b
     else:
         raise ZeroDivisionError
+    

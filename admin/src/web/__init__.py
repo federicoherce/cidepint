3782d177c953e5715c9214d552b90e55638127fc
Federico Herce <federicoherce25@gmail.com>
2023-09-10 23:16:46 -0300

    adding templates and styles

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-def create_app():
-    app = Flask(__name__)
+from flask import render_template
+
+
+def create_app(env="development", static_folder="../../static"):
+    app = Flask(__name__, static_folder=static_folder)
     app.get("/")
 
     @app.get("/")
     def home():
-        return "Hola Mundo!"
+        return render_template("home.html")
 
     return app

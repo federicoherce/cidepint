9f2a31df44959ee3a34ff4cba0add3c90661a649
Mercado Bautista <mercadob267@gmail.com>
2023-09-19 17:30:47 -0300

    Fix db config

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template
 from src.web import error
-from src.core import database
+from src.core import database, seeds
+from src.web.config import config
 
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
+    app.config.from_object(config[env])
     app.get("/")
 
     database.init_app(app)
@@ -13,6 +15,14 @@ def create_app(env="development", static_folder="../../static"):
     @app.get("/")
     def home():
         return render_template("home.html")
+    
+    @app.cli.command(name="resetdb")
+    def resetdb():
+        database.reset_db()
+
+    @app.cli.command(name="seedsdb")
+    def seedsdb():
+        seeds.run()
     
     app.register_error_handler(404, error.not_found_error)
     

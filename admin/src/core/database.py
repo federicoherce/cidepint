85721dc92411b21524e7177ac6e22cff54471624
Mercado Bautista <mercadob267@gmail.com>
2023-09-19 17:07:37 -0300

    Add db configuration

---
+++ admin/src/core/database.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+
+database = SQLAlchemy()
+
+def init_app(app):
+	"""
+	Inicialización de la aplicación
+	"""
+	database.init_app(app)
+	config_db(app)
+
+# Configuración de la DB
+def config_db(app):
+	"""
+	Configuración de la app.
+	
+	close_session -> Cuando se termina un request (teardown), se cierra la conexión
+	con la DB que se abrió
+	"""
+	@app.teardown_request
+	def close_session(exception=None):
+		database.session.close()
+
+def reset_db():
+	"""
+	Reseteo de la DB.
+
+	Lo usamos auxiliarmente. Flask me permite hacer comandos, ejecutaríamos esto con dicho comando
+	"""
+	print("Eliminando DB")
+	database.drop_all()
+	print("Creando DB")
+	database.create_all()
---
+++ admin/src/web/config.py
@@ -0,0 +1,48 @@
+from os import environ
+
+
+class Config(object):
+    #Clase base
+
+    SECRET_KEY = "secret"
+    TESTING = False
+    SESSION_TYPE = "filesystem"
+
+
+class ProductionConfig(Config):
+
+    DB_USER = environ.get("DB_USER")
+    DB_PASS = environ.get("DB_PASS")
+    DB_HOST = environ.get("DB_HOST")
+    DB_NAME = environ.get("DB_NAME")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+    )
+
+
+class DevelopmentConfig(Config):
+    #Configuracion de desarollo
+
+    DB_USER = "postgres"
+    DB_PASS = "postgres"
+    DB_HOST = "localhost"
+    DB_NAME = "pruebas"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+    )
+
+
+
+class TestingConfig(Config):
+    #Configuracion de testeo
+
+    TESTING = True
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "test": TestingConfig,
+}

94a68710d130574bdaf23c3a815a02200a34fc69
NOVOMISKI MATEO MAXIMO <mateonovo1@gmail.com>
2023-10-10 18:21:00 -0300

    agrego en el init

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -7,6 +7,8 @@ import logging
 from src.web import routes
 from src.web import commands
 from src.web import error_handlers
+from flask import session, abort
+from src.web.helpers.auth import has_permissions
 
 #logging.basicConfig()
 #logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
@@ -26,7 +28,15 @@ def create_app(env="development", static_folder="../../static"):
     routes.register_routes(app)
     commands.register_commands(app)
     error_handlers.register_errors(app)
-
+    @app.before_request
+    def check_maintenance_mode():
+        print("MODO EN EL INIT???!!!!!!!@@@@??" ,app.config['MAINTENANCE_MODE'])
+        if app.config['MAINTENANCE_MODE'] and (not session.get("user_id") or not has_permissions(['user_index'])):
+            return abort(503) 
+        
+    @app.errorhandler(503)
+    def error_503(error):
+        return render_template("maintenance.html")
     app.jinja_env.globals.update(is_authenticated=auth.is_authenticated)
 
     return app

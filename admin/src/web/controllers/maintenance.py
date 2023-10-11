afc1ead49d1cfa93ff039acb3eeb0368f97b78f6
NOVOMISKI MATEO MAXIMO <mateonovo1@gmail.com>
2023-10-11 09:07:16 -0300

    mensaje de desactivar mantenimiento correct

--- admin/src/web/controllers/maintenance.py
+++ admin/src/web/controllers/maintenance.py
@@ -19,7 +19,7 @@ def toggle_maintenance():
             if action == 'Activar Mantenimiento':
                 app.config['MAINTENANCE_MODE'] = True
                 flash("Se activó modo Mantenimiento: " + str(app.config['MAINTENANCE_MODE']), "info")
-            elif action == 'Desactivar mantenimiento':
+            elif action == 'Desactivar Mantenimiento':
                app.config['MAINTENANCE_MODE']  = False
                flash("Se deactivó modo mantenimiento: " + str(app.config['MAINTENANCE_MODE']), "info")
     return render_template('maintenance_form.html')

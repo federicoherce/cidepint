eac32245954a0eecd2bcacf6a7c2a24df961dc61
NOVOMISKI MATEO MAXIMO <mateonovo1@gmail.com>
2023-10-12 13:30:13 -0300

    actualice el maintenance con flask forms y agregue nuevo permiso para maintenance

--- admin/src/web/controllers/maintenance.py
+++ admin/src/web/controllers/maintenance.py
@@ -3,30 +3,32 @@ from src.core import auth
 from src.web.helpers.auth import login_required, has_permissions
 from src.web.config  import config
 from flask import current_app as app
-
+from forms.maintenance_form import MaintenanceForm
 
 maintenance_bp = Blueprint("maintenance", __name__, url_prefix="/maintenance")
 
 
 @maintenance_bp.get('/')
 def index():
-    return render_template('maintenance_form.html')
+    return render_template('maintenance_form.html',form = MaintenanceForm())
 
 
 @maintenance_bp.before_request
+@login_required
 def not_has_permissions():
-    if not has_permissions(['user_show']):
+    if not has_permissions(['user_maintenance']):
            abort(401)  # Acceso prohibido si no es un super admin
 
 
 
 @maintenance_bp.post('/toggle')
 def toggle_maintenance():   
-    action = request.form['action']
-    if action == 'Activar Mantenimiento':
-        app.config['MAINTENANCE_MODE'] = True
-        flash("Se activó modo Mantenimiento: " + str(app.config['MAINTENANCE_MODE']), "info")
-    elif action == 'Desactivar Mantenimiento':
-       app.config['MAINTENANCE_MODE']  = False
-       flash("Se deactivó modo mantenimiento: " + str(app.config['MAINTENANCE_MODE']), "info")
-    return redirect(url_for('maintenance.index'))
+    form = MaintenanceForm()
+    if form.validate_on_submit():
+        if form.activate_maintenance.data:
+            app.config['MAINTENANCE_MODE'] = True
+            flash("Se activó modo Mantenimiento: " + str(app.config['MAINTENANCE_MODE']), "info")
+        elif form.deactivate_maintenance.data:
+            app.config['MAINTENANCE_MODE']  = False
+            flash("Se deactivó modo mantenimiento: " + str(app.config['MAINTENANCE_MODE']), "info")
+        return redirect(url_for('maintenance.index'))

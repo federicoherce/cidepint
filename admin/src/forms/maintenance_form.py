b353f66667f794e7db9225d86dedbba9b7284889
NOVOMISKI MATEO MAXIMO <mateonovo1@gmail.com>
2023-10-14 20:50:58 -0300

    agrego informacion de contacto

--- admin/src/forms/maintenance_form.py
+++ admin/src/forms/maintenance_form.py
@@ -6,3 +6,10 @@ from wtforms.validators import DataRequired, Email, Length
 class MaintenanceForm(FlaskForm):
     activate_maintenance = SubmitField('Activar Mantenimiento')
     deactivate_maintenance = SubmitField('Desactivar Mantenimiento')
+    
+    
+class ContactoForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(message="Este campo es obligatorio"),
+                                    Email(message="El mail ingresado no es válido.")])
+    guardar = SubmitField('guardar')
--- admin/src/web/controllers/maintenance.py
+++ admin/src/web/controllers/maintenance.py
@@ -5,6 +5,7 @@ from src.web.helpers.maintenance import maintenance
 from src.web.config  import config
 from flask import current_app as app
 from forms.maintenance_form import MaintenanceForm
+from forms.maintenance_form import ContactoForm
 
 maintenance_bp = Blueprint("maintenance", __name__, url_prefix="/maintenance")
 
@@ -29,3 +30,15 @@ def toggle_maintenance():
             app.config['MAINTENANCE_MODE']  = False
             flash("Se deactivó modo mantenimiento: ", "info")
         return redirect(url_for('maintenance.index'))
+    
+
+@maintenance_bp.get('/contacto')
+def index_contacto():
+    form = ContactoForm()
+    return render_template('configuraciones/info_contacto.html' , form = form )    
+
+
+@maintenance_bp.post('/contacto')
+def info_contacto():
+    flash("Se guardaron los cambios correctamente", "info")
+    return redirect(url_for('maintenance.index_contacto'))

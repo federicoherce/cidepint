42e202f472c877e31ba487a69a3dfef922804ed1
gianquaranta <gianq7@gmail.com>
2023-10-16 20:06:32 -0300

    limpio solicitudes

--- admin/src/core/services/__init__.py
+++ admin/src/core/services/__init__.py
@@ -2,8 +2,6 @@ from src.core.services.services import Servicio, Solicitud
 from src.core.database import database as db
 
 
-# ----------------------- SERVICIOS
-
 def create_service(**kwargs):
     service = Servicio(**kwargs)
     db.session.add(service)
@@ -25,24 +23,4 @@ def update_service(form, service):
     
 def delete_service(service):
     db.session.delete(service)
-    db.session.commit()
-
-# ------------------------ SOLICITUDES
-
-def list_solicitudes():
-    solicitudes = Solicitud.query.all()
-    return solicitudes
-
-def show_solicitud(id):
-    solicitud = Solicitud.query.filter_by(id=id).first()
-    return solicitud
-
-def update_solicitud(solicitud, **kwargs):
-    for key, value in kwargs.items():
-        if hasattr(solicitud, key):
-            setattr(solicitud, key, value)
-    db.session.commit()
-
-def delete_solicitud(solicitud):
-    db.session.delete(solicitud)
     db.session.commit()
--- admin/src/core/services/services.py
+++ admin/src/core/services/services.py
@@ -20,25 +20,3 @@ class Servicio(db.Model):
         self.centros = centros
         self.tipo_servicio = tipo_servicio
         self.habilitado = habilitado
-
-
-
-class Solicitud(db.Model):
-    __tablename__ = "solicitudes"
-    id = db.Column(Integer, primary_key=True, unique=True)
-    servicio_id = db.Column(db.Integer, db.ForeignKey("servicios.id"))
-    cliente_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    detalles = db.Column(String(500), nullable = True)
-    estado = db.Column(db.String(20), nullable = False, default='EN PROCESO')  # Estados: aceptada, rechazada, en proceso, finalizada, canceladaa
-    fecha_creacion = db.Column(db.DateTime, default=datetime.utcnow)
-    fecha_cambio_estado = db.Column(db.DateTime, default=datetime.utcnow)
-    observacion_cambio_estado = db.Column(db.String(200), default='')
-    # Falta el campo de archivos adjuntos
-    cliente = db.relationship('User', backref='solicitudes')
-    servicio = db.relationship('Servicio', backref='solicitudes')
-
-    def __init__(self, servicio_id, cliente_id, detalles):
-        self.servicio_id = servicio_id
-        self.cliente_id = cliente_id
-        detalles = detalles
-        

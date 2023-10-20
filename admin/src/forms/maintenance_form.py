e6a7a8017f6d76b091a60aab6409369141c97dd5
Federico Herce <federicoherce25@gmail.com>
2023-10-20 15:06:34 -0300

    cambios en el layout e info de contacto

--- admin/src/forms/maintenance_form.py
+++ admin/src/forms/maintenance_form.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, IntegerField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email
 
 
 class MaintenanceForm(FlaskForm):
     activate_maintenance = SubmitField('Activar Mantenimiento')
     deactivate_maintenance = SubmitField('Desactivar Mantenimiento')
-    mensaje = StringField('Mensaje')
+    mensaje = StringField('Mensaje de mantenimiento')
     guardar = SubmitField('Guardar Mensaje')
 
 
@@ -19,5 +19,6 @@ class ContactoForm(FlaskForm):
 
 
 class paginadoForm(FlaskForm):
-    per_page = IntegerField('Cantidad de elementos por pagina', validators=[DataRequired(message="Este campo es obligatorio")])
+    per_page = IntegerField('Cantidad de elementos por pagina',
+                            validators=[DataRequired(message="Este campo es obligatorio")])
     guardar = SubmitField('Guardar')

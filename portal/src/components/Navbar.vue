953efb808580fd6fcdd9559821d6c08815ae9c2d
Federico Herce <federicoherce25@gmail.com>
2023-11-01 20:04:21 -0300

    home app publica

---
+++ portal/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav>
+      <ul>
+        <li><router-link to="/">Inicio</router-link></li>
+        <li><router-link to="/about">Acerca de</router-link></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar'
+  }
+  </script>
+  
+  <style scoped>
+nav {
+  background-color: #333;
+  color: #fff;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  text-decoration: none;
+  color: #fff;
+}
+</style>

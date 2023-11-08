87b2adf2f1ee36fcdbac6da3a1b7fcf9d3a27100
Federico Herce <federicoherce25@gmail.com>
2023-11-08 00:27:37 -0300

    cambio estilos y mapa

--- portal/src/views/detallesServicio.vue
+++ portal/src/views/detallesServicio.vue
@@ -23,10 +23,10 @@
       <li> Direccion: {{ institucion.direccion }} </li>
       <li> Localización: {{ institucion.localizacion }} </li>
       <li> Horarios: {{ institucion.horarios }} </li>
-      <li> Contacto: {{ institucion.contacto }} </li>
-      <li v-if="institucion.habilitado"> Habilitado: Si </li>
+      <li> Contacto: {{ institucion.contacto }} - va en mapa </li>
+      <li v-if="institucion.habilitado"> - Habilitado: Si </li>
       <li v-else> Habilitado: No </li>
-      <li>  Web: {{ institucion.web }} </li>
+      <li>  Web: {{ institucion.web }} - va en mapa </li>
     </ul>
   </div>
   <br>
@@ -35,77 +35,12 @@
 </template>
 
 <script>
-import { apiService } from '@/api';
+import detalleServicio from './javascript/detalleServicio.js';
 
-export default {
-  props: ['id'],
-  data() {
-    return {
-      servicio: {},
-      institucion: {},
-    };
-  },
-  mounted() {
-    this.obtenerServicio();
-    const map = L.map('mapa').setView([51.505, -0.09], 13); // Coordenadas iniciales y zoom
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-  },
-  methods: {
-    async obtenerServicio() {
-      try {
-        const respuesta = await apiService.get(`/api/services/${this.id}`);
-        this.servicio = respuesta.data;
-
-        // Llama a la función para obtener la institución
-        await this.obtenerInstitucion(this.servicio.institucion_id);
-      } catch (error) {
-        console.error('Error al obtener la información del servicio', error);
-      }
-    },
-    async obtenerInstitucion(institucionId) {
-      try {
-        const respuesta = await apiService.get(`/api/instituciones/${institucionId}`);
-        this.institucion = respuesta.data;
-      } catch (error) {
-        console.error('Error al obtener la información de la institución', error);
-      }
-    },
-  },
-
-};
+export default detalleServicio;
 </script>
 
 <style>
-.detalles {
-max-width: 600px;
-margin: 0 auto;
-padding: 50px;
-background-color: #ebdada;
-border-radius: 10px;
-box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
+@import './css/detalleServicio.css';
 
-.detalles h2 {
-font-size: 1.5em;
-margin-bottom: 10px;
-}
-
-.detalles li {
-font-size: 1.2em;
-line-height: 1.6;
-}
-
-button {
-  font-size: 1em;
-  padding: 10px 20px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
 </style>
---
+++ portal/src/views/javascript/detalleServicio.js
@@ -0,0 +1,42 @@
+import { apiService } from '@/api';
+
+export default {
+  props: ['id'],
+  data() {
+    return {
+      servicio: {},
+      institucion: {},
+    };
+  },
+  mounted() {
+    this.obtenerServicio();
+    const map = L.map('mapa').setView([-34.91895294495346, -57.95574638183875], 14);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+  },
+  methods: {
+    async obtenerServicio() {
+      try {
+        const respuesta = await apiService.get(`/api/services/${this.id}`);
+        this.servicio = respuesta.data;
+
+        await this.obtenerInstitucion(this.servicio.institucion_id);
+      } catch (error) {
+        console.error('Error al obtener la información del servicio', error);
+      }
+    },
+    async obtenerInstitucion(institucionId) {
+      try {
+        const respuesta = await apiService.get(`/api/instituciones/${institucionId}`);
+        this.institucion = respuesta.data;
+      } catch (error) {
+        console.error('Error al obtener la información de la institución', error);
+      }
+    },
+  },
+
+};
